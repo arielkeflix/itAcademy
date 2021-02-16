503a2b64f5b3140872e0f60c1fdbf690b0667b55
ariel <arielkeflix@gmail.com>
2021-02-16 18:37:28 +0100

    Ejercicios Modulo 1

---
+++ src/modulo1/Mod1Nivel1Fase1.java
@@ -0,0 +1,15 @@
+package modulo1;
+
+public class Mod1Nivel1Fase1 {
+
+	public static void main(String[] args) {
+		String nombre = "Ariel";
+		String apellido1 = "Liffschitz";
+		String apellido2 = "Cohan";
+		
+		System.out.println("El nombre completo es: "+ nombre + " "+apellido1+" "+ apellido2);
+		System.out.println("El nombre completo es: "+ nombre + "/"+apellido1+"/"+ apellido2);
+
+	}
+
+}
